95bf94a33a193bae3b15fa9a8a243df2ba002b05
stholen <saxemerson@gmail.com>
2020-10-18 15:11:54 -0300

    creat first route for express test

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", (request,response) => {
+    response.json({
+        message:"Hello World"
+    })
+})
+
+app.listen(3333);
